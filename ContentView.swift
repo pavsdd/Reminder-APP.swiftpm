7a3e1fe6bbb086f5985db17076cb1f34e40420b5
Jake <jvoris6444@stu.d214.org>
2023-05-02 14:52:15 -0500

    Added descriptions to nAVIgation links

--- ContentView.swift
+++ ContentView.swift
@@ -8,18 +8,22 @@ struct ContentView: View {
                     .italic()
                     .fontWeight(.bold)
                     .font(.system(size: 36))
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(.blue)
                 Spacer()
                 
                 NavigationLink("List"){
                     ListView()
+                        .frame(width: 50, height: 50)  .foregroundColor(.blue)
                 }
                 
                 Spacer()
                     NavigationLink("Calendar"){
                         CalendarView()
                     }
+                    .frame(width: 50, height: 50) .foregroundColor(.blue)
                 Spacer()
-                
+                    
             }
            
         }
