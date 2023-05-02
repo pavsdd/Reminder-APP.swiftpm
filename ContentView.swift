d07298c136cc32f903d78acb2afefcf0c2cd0b83
Ben Wojtowicz <Bwojtowicz4483@stu.d214.org>
2023-05-02 13:58:16 -0500

    content vein fwd

--- ContentView.swift
+++ ContentView.swift
@@ -2,22 +2,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-           Text("Reminder App")
-                .italic()
-                .fontWeight(.bold)
-                .baselineOffset(1000)
-                .font(.system(size: 36))
-            }
-        VStack{
-            NavigationLink("List View"){
-                ListView()
-            }
-           
-        }
-        VStack{
-            NavigationLink("Calendar View"){
-                CalendarView()
+        NavigationView{
+            VStack {
+                Text("Reminder App")
+                    .italic()
+                    .fontWeight(.bold)
+                    .font(.system(size: 36))
+                Spacer()
+                
+                NavigationLink("List"){
+                    ListView()
+                }
+                
+                Spacer()
+                    NavigationLink("Calendar"){
+                        CalendarView()
+                    }
+                Spacer()
+                
             }
            
         }
