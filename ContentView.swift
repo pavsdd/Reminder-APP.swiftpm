60bfafae56a542b191dc42df389e1f85b8c1bc66
Ben Wojtowicz <Bwojtowicz4483@stu.d214.org>
2023-05-24 13:47:37 -0500

    content view

--- ContentView.swift
+++ ContentView.swift
@@ -1,63 +1,47 @@
 import SwiftUI
-import UserNotifications
 struct ContentView: View {
+     @Binding var events: [EventDescription]
+    @State var Date1 = Date()
     var body: some View {
-    NavigationView{
+        NavigationView{
             VStack {
                 Text("Reminder App")
-                    .font(.custom("chalkboard", size: 50))
-                    .frame(width: 300, height: 75)
-                    .foregroundColor(.blue)
-                
+                    .font(.custom("chalkboard", size: 30))
+                    .frame(width: 200, height: 50)
+                    
                 Spacer()
                 
                 NavigationLink {
                     ListView()
                 } label: {
                     Text("List")
-                        .frame(width: 100, height: 100)
-                        .font(.custom("chalkboard", size: 45))
-                        .foregroundColor(.red)
+                        .frame(width: 250
+                           , height: 75)
+                        .font(.custom("chalkboard", size: 30))
+                        .background(.red)
+                        .foregroundColor(.white)
                 }
                 
                 
                 Spacer()
                 
                 NavigationLink {
-                    CalendarView()
+                    CalendarView(Date1:Date1)
                 } label: {
-                    Text("Calendar")
-                        .frame(width: 200, height: 100)
-                        .font(.custom("chalkboard", size: 45))
-                        .foregroundColor(.blue)
+           Text("Calendar")
+                        .frame(width: 250
+                               , height: 75)
+                        .font(.custom("chalkboard", size: 30))
+                        .background(.blue)
+                        .foregroundColor(.white)
                 }
-                Text("")
-                    .toolbar {
-                        ToolbarItem(placement: .bottomBar) {
-                            NavigationLink { 
-                                ListView()
-                            } label: { 
-                                Label("List", systemImage: "clipboard.fill")
-                                    .foregroundColor(.blue)
-                            }
-                            
-                        }
-                        ToolbarItem(placement: .bottomBar){
-                            NavigationLink { 
-                                CalendarView()
-                            } label: { 
-                                Label("List", systemImage: "calendar.badge.clock")
-                                    .foregroundColor(.red)
-                            }
-                            
-                        }
-                    }
                 
                 
                 Spacer()
                 
             }
-            }
-    .navigationViewStyle(.stack)
+            
         }
+        .navigationViewStyle(.stack)
     }
+}
